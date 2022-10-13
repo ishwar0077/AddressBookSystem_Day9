01fe046d9ff09a9529c411cce0c5bfd28a4de6a5
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-13 21:04:15 +0530

    Creating Address Book for 1 Person

---
+++ AddressBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemProgram
+{
+    public class AddressBook
+    {
+        public void CreateAddressBook(string fistName, string lastName, 
+                                        string addrss, string city, string state,
+                                        string zipcode, string phoneNumber, string email )
+        {
+            Console.WriteLine("First Name: "+fistName
+                                +"\nLast Name: "+lastName
+                                +"\nAddress: "+addrss
+                                +"\nCity: "+ city
+                                +"\nState: "+state
+                                +"\nZip Code: "+zipcode
+                                +"\nPhone Number: "+ phoneNumber
+                                +"\nEmail: "+email);
+           
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            AddressBook person = new AddressBook();
+            person.CreateAddressBook("Ishwar", "Rathod","Telgaon", "Wadwani", "Maharastra", "4321131", "9146422222", "ishwar.demo@gmail.com");
         }
     }
 }
